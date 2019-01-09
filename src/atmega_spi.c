c5ab962df93a01522ea42bb7c283355ea5883901
Ben Baker-Smith <bbakersmith@gmail.com>
2019-01-09 02:21:34 -0600

    Minor cleanup

--- src/atmega_spi.c
+++ src/atmega_spi.c
@@ -7,24 +7,6 @@
 #define SCK PORTB5
 #define SS PORTB2
 
-void atmega_spi_master_init(void) {
-  // set CS high
-  PORTB |= (1 << SS);
-  // Set MOSI, SCK, and SS output, all others input
-  DDRB = (1 << MOSI) | (1 << SCK) | (1 << SS);
-  // Enable SPI, saster, set clock rate fck/16
-  SPCR = (1 << SPE) | (1 << MSTR) | (0 << SPR1) | (1 << SPR0);
-}
-
-void atmega_spi_slave_init(void) {
-  // Set MISO output, all others input
-  DDRB = (1 << PINB4);
-  // Enable SPI, slave, interrupt enable
-  SPCR = (1 << SPE) | (1 << SPIE);
-  // Preset SPI data register to 0
-  SPDR = 0;
-}
-
 void atmega_spi_queue_transfer(uint8_t data) {
   SPDR = data;
 }
@@ -43,4 +25,22 @@ uint8_t atmega_spi_receive(void) {
   while(!(SPSR & (1 << SPIF)));
   // Return data from MISO?
   return(SPDR);
+}
+
+void atmega_spi_master_init(void) {
+  // set CS high
+  PORTB |= (1 << SS);
+  // Set MOSI, SCK, and SS output, all others input
+  DDRB = (1 << MOSI) | (1 << SCK) | (1 << SS);
+  // Enable SPI, saster, set clock rate fck/16
+  SPCR = (1 << SPE) | (1 << MSTR) | (0 << SPR1) | (1 << SPR0);
+}
+
+void atmega_spi_slave_init(void) {
+  // Set MISO output, all others input
+  DDRB = (1 << PINB4);
+  // Enable SPI, slave, interrupt enable
+  SPCR = (1 << SPE) | (1 << SPIE);
+  // Preset SPI data register to 0
+  atmega_spi_queue_transfer(0);
 }
